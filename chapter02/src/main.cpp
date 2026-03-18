646ea0e94bb7b29c73a5882fb808e30ab1417e80
lEe_NiAnsAn <123313005+lEe-NiAnsAn@users.noreply.github.com>
2026-03-18 14:40:27 +0800

    elements buffer object

--- chapter02/src/main.cpp
+++ chapter02/src/main.cpp
@@ -109,8 +109,8 @@ int main() {
     glDeleteShader(fragmentShader); // 释放片段着色器对象
 
 
-    // 设置顶点数组（采用标准化设备坐标来表示）
-    float vertices[] = {
+    // 设置顶点数组（采用标准化设备坐标来表示,在视口改变尺寸时可能会变形）
+    float vertices01[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
@@ -122,7 +122,7 @@ int main() {
     glBindVertexArray(VAO01);   // 绑定至当前缓冲数组（进入“状态机”，储存接下来的设置）
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO01);   // 绑定至相应类型（可同时绑定多个不同类型的缓冲）
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  // 将顶点数据复制至当前绑定的VBO（ GL_STATIC_DRAW 标志为告知计算机该数据几乎不会改变）
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices01), vertices01, GL_STATIC_DRAW);  // 将顶点数据复制至当前绑定的VBO（ GL_STATIC_DRAW 标志为告知计算机该数据几乎不会改变）
     
     // 定义顶点属性的数据格式和布局（输入变量位置值，单个“属性”的组成大小，“属性”组成值的类型，是否标准化，步长，起始位置偏移量）
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -130,6 +130,32 @@ int main() {
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);   // 解绑当前 VBO
     glBindVertexArray(0);   // 解绑当前 VAO
+
+    // 元素缓冲对象
+    float vertices02[] = {    // 通过两个三角形绘制正方形
+        -0.2f, 0.7f, 0.0f, // 右上角
+        -0.2f, 0.2f, 0.0f, // 右下角（通过EBO这个缓冲区避免了顶点的重复）
+        -0.7f, 0.7f, 0.0f, // 左下角
+        -0.7f, 0.2f, 0.0f  // 左上角（避免重复）
+    };
+    unsigned int indices[] = {  // 通过索引确定各个三角的顶点值
+        0, 1, 2, 
+        1, 2, 3
+    };
+    unsigned int VAO02, EBO01, VBO02;
+    glGenVertexArrays(1, &VAO02);
+    glGenBuffers(1, &EBO01);    // 生成元素缓冲对象
+    glGenBuffers(1, &VBO02);
+    glBindVertexArray(VAO02);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO02);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices02), vertices02, GL_STATIC_DRAW);  // 复制顶点数据至 VBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO01);   // 绑定
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // 复制
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);   // 设置顶点属性
+    glEnableVertexAttribArray(0);   // 输入变量位置值定位
+    // 不可提前解绑 VAO ：当目标为 GL_ELEMENT_ARRAY_BUFFER 时，VAO 会储存 glBindBuffer 的函数调用，其中便储存了解绑调用（即将 EBO 的绑定记录为“解绑”）
+    glBindBuffer(GL_ARRAY_BUFFER, 0);   // 解绑 VBO
+    glBindVertexArray(0);   // 解绑 VAO
 
     while (!glfwWindowShouldClose(window)){
 		processInput(window);
@@ -141,6 +167,10 @@ int main() {
         glUseProgram(shaderProgram);    // 激活着色器程序
         glBindVertexArray(VAO01);   // 使用该 VAO 配置进行渲染
         glDrawArrays(GL_TRIANGLES, 0, 3);   // 绘制顶点数组内容（图元类型，起始索引，绘制顶点数）
+		glBindVertexArray(VAO02);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	// 设置光栅化模式（指定作用面，指定模式）
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);    // 绘制缓冲元素（图元类型，顶点个数，索引类型，偏移量）
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);	// 设置回初始值
 
 		glfwSwapBuffers(window);
         glfwPollEvents();
@@ -148,6 +178,8 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO01);
     glDeleteBuffers(1, &VBO01);
+	glDeleteBuffers(1, &VAO02);
+	glDeleteBuffers(1, &EBO01);
     glDeleteProgram(shaderProgram);
 
     
